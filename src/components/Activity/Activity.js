5981455401006f315d00b39e8649d044dda964ae
Gustavo Koch <guzko@hotmail.com>
2022-04-16 15:12:57 +0200

    Last refactoring changes with Ben in App putting the 4 functions into 1

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,34 +1,57 @@
 import React from "react";
 
-export default function Activity({ id, desc, cat, checkIfDone, editing, onDeleteTask, toggleEditTask, toggleDone, editInput, comboChange }) {
-
+export default function Activity({
+  id,
+  desc,
+  cat,
+  checkIfDone,
+  editing,
+  onDeleteTask,
+  editInput,
+  comboChange,
+  onMixedChanges,
+}) {
   return (
     <li key={id}>
-      <input name={id} type="text" defaultValue={desc} disabled={!editing} onChange={editInput}/>
-      
-    <label htmlFor="toDos"></label>
-    <select id="toDos" name={id} onChange={comboChange} value={cat}>
-      <option value="Urgent">🚨</option>
-      <option value="Important">🔥</option> 
-      <option value="Idea">💡</option>
-      <option value="House">🏠</option>
-      <option value="House">🍔</option>
-      <option value="Shopping">🛒</option>
-      <option value="Sport">⚽</option>
-      <option value="Doctor">👨‍⚕️</option>
-      <option value="Holiday">🏖️</option>          
-      <option value="Reminders">🧠</option>
-      <option value="Idea">💡</option>      
-      <option value="Others">🌈</option>
-    </select>
-
-
-      <button onClick={() => toggleEditTask(id)}>{editing?"🔓":"🔒"}</button>
-      <button onClick={() => onDeleteTask(id)}>🗑️</button>
       <input
-        name="check"
+        name="desc"
+        type="text"
+        defaultValue={desc}
+        disabled={!editing}
+        onChange={(e) => onMixedChanges(e, id)}
+      />
+
+      <label htmlFor="toDos"></label>
+      <select
+        id="toDos"
+        name="category"
+        onChange={(e) => onMixedChanges(e, id)}
+        value={cat}
+      >
+        <option value="Urgent">🚨</option>
+        <option value="Important">🔥</option>
+        <option value="Idea">💡</option>
+        <option value="House">🏠</option>
+        <option value="House">🍔</option>
+        <option value="Shopping">🛒</option>
+        <option value="Sport">⚽</option>
+        <option value="Doctor">👨‍⚕️</option>
+        <option value="Holiday">🏖️</option>
+        <option value="Reminders">🧠</option>
+        <option value="Idea">💡</option>
+        <option value="Others">🌈</option>
+      </select>
+
+      <button name="editing" onClick={(e) => onMixedChanges(e, id)}>
+        {editing ? "🔓" : "🔒"}
+      </button>
+      <button onClick={(e) => onDeleteTask(e, id)}>🗑️</button>
+      <input
+        name="checkIfDone"
         type="checkbox"
-        onChange={() => {toggleDone(id)}}
+        onChange={(e) => {
+          onMixedChanges(e, id);
+        }}
         checked={checkIfDone}
       />
     </li>
